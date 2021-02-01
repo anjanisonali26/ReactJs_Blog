935fcf006a00c7cba5252a2e153c1759c6f6e535
anjanisonali <anjanibendre260201@gmail.com>
2021-02-01 22:08:58 +0700

    learn reactjs

---
+++ src/assets/components/image.js
@@ -0,0 +1,14 @@
+import react , {Component} from 'react'
+
+export default class image extends Component{
+
+state ={
+    img=[
+        "https://i.pinimg.com/originals/d2/e5/35/d2e5359f8402cb8d3d7b22c463f9013b.png",
+        
+    ]
+}
+
+
+
+}
